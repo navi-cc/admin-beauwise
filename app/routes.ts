4403ce82004887718317a1f7c87d08ff870bc8fe
navi-cc <157870465+navi-cc@users.noreply.github.com>
2026-04-20 11:43:16 +0800

    Update global style, root file and routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from '@react-router/dev/routes';
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+	layout('routes/app-layout.jsx', [
+		index('routes/tabs/learn.jsx'),
+		route('user-management', 'routes/tabs/user-management.jsx')
+	])
+] satisfies RouteConfig;
